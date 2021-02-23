b25698507cd179b6faaf5464ce2ab8cb3c130b12
Chirantan123 <chirantanism@gmail.com>
2021-02-23 18:27:53 +0530

    Redis post commit

--- src/main/java/com/example/HibernateDemo/Entity/Employee.java
+++ src/main/java/com/example/HibernateDemo/Entity/Employee.java
@@ -3,21 +3,15 @@ package com.example.HibernateDemo.Entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.redis.core.RedisHash;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
-@Entity(name = "interns")
+
+@RedisHash(value="Employee")
 @Getter
 @Setter
 public class Employee {
 
-    @Id
-    @GenericGenerator(name = "employee_id_seq", strategy = "increment")
-    @GeneratedValue(generator = "employee_id_seq", strategy = GenerationType.AUTO)
     private long id;
 
     private  String name;
