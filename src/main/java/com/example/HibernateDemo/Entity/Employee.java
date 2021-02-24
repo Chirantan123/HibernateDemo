73b09fa58d9495bfd5c5290443bf15925686457d
Chirantan123 <chirantanism@gmail.com>
2021-02-24 10:11:37 +0530

    redis 2nd commit

--- src/main/java/com/example/HibernateDemo/Entity/Employee.java
+++ src/main/java/com/example/HibernateDemo/Entity/Employee.java
@@ -17,4 +17,28 @@ public class Employee {
     private  String name;
 
     private String departmentName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
 }
